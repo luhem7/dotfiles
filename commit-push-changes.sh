711ae30b5937001d74672836a5ecc9f353afe991
luhem7 <mehul.gang@gmail.com>
2024-02-18 09:16:50 -0500

    Made commit-push-changes only act when in a git directory

--- commit-push-changes.sh
+++ commit-push-changes.sh
@@ -1,4 +1,11 @@
-git status
-git add .
-git commit -a --allow-empty-message -m ''
-git push
+#! /bin/sh
+
+if [ -d .git ] || git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    git status
+    git add .
+    git commit -a --allow-empty-message -m ''
+    git push
+else
+    echo "You are not in a Git repository."
+fi
+
