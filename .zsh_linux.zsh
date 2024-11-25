3477816cd0834cd4e77933a25420be87b365e160
luhem7 <mehul.gang@gmail.com>
2024-11-25 13:02:53 -0500

    Added letting locally install applications show up on the path in linux.

--- .zsh_linux.zsh
+++ .zsh_linux.zsh
@@ -7,3 +7,6 @@ lsd_install_path=~/.cargo/bin/lsd
 if [ -f "$lsd_install_path" ]; then
     alias ls="$lsd_install_path"
 fi
+
+# Source locally installed applications
+export PATH=$HOME/.local/bin:$HOME/applications:$PATH
