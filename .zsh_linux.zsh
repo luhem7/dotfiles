b601e2c7c1b83f58997b6547b4608c8763af0f92
luhem7 <mehul.gang@gmail.com>
2025-08-26 07:34:28 -0400

    Added a little helper command to update all the popos dependencies

--- .zsh_linux.zsh
+++ .zsh_linux.zsh
@@ -2,6 +2,8 @@
 
 alias nvim='flatpak run io.neovim.nvim'
 
+alias popos_upgrade_all='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && flatpak update -y'
+
 # Alias ls to lsd if lsd is installed.
 lsd_install_path=~/.cargo/bin/lsd
 if [ -f "$lsd_install_path" ]; then
