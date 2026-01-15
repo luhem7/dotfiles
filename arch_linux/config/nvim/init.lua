dee5060c692a676dc3ac81df4cfef7b753e522c6
luhem7 <mehul.gang@gmail.com>
2026-01-15 07:00:16 -0500

    Ensuring we use 4 spaces for indents instead of tabs

--- arch_linux/config/nvim/init.lua
+++ arch_linux/config/nvim/init.lua
@@ -7,5 +7,11 @@ require("config.lazy")
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Indentation: tabs, 4 spaces wide
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
 -- Keybindings
 vim.keymap.set('n', '<leader>rs', ':source $MYVIMRC | echo "Config reloaded!"<CR>', { desc = 'Reload System Config' })
