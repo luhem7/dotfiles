fef5eed695aa4d0fda63369c29cfdbe5963570d8
luhem7 <mehul.gang@gmail.com>
2026-01-16 06:33:49 -0500

    Made the clock look like the bottom of a terminal off the top of the screen

--- arch_linux/config/ags/widget/Bar.tsx
+++ arch_linux/config/ags/widget/Bar.tsx
@@ -4,41 +4,43 @@ import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const time = createPoll("", 1000, "date '+%A %Y-%m-%d %I:%M:%S %p'")
-  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+	const time = createPoll("", 1000, "date '+%A %Y-%m-%d %I:%M:%S %p'")
+	const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-  return (
-    <window
-      visible
-      name="bar"
-      class="Bar"
-      gdkmonitor={gdkmonitor}
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
-      anchor={TOP | LEFT | RIGHT}
-      application={app}
-    >
-      <centerbox cssName="centerbox">
-        <box $type="start" />
-        {/* <button
-          $type="start"
-          onClicked={() => execAsync("echo hello").then(console.log)}
-          hexpand
-          halign={Gtk.Align.CENTER}
-        >
-          <label label="Welcome to AGS!" />
-        </button> */}
-        <box $type="center" />
-        <box $type="end" hexpand halign={Gtk.Align.END}>
-          <label class="chevron" valign={Gtk.Align.CENTER} label="" />
-          <menubutton class="clock">
-            <label label={time} />
-            <popover>
-              <Gtk.Calendar />
-            </popover>
-          </menubutton>
-          <label class="chevron" valign={Gtk.Align.CENTER} label="" />
-        </box>
-      </centerbox>
-    </window>
-  )
+	return (
+		<window
+			visible
+			name="bar"
+			class="Bar"
+			gdkmonitor={gdkmonitor}
+			exclusivity={Astal.Exclusivity.EXCLUSIVE}
+			anchor={TOP | LEFT | RIGHT}
+			application={app}
+		>
+			<centerbox cssName="centerbox">
+				<box $type="start" />
+				{/* <button
+					$type="start"
+					onClicked={() => execAsync("echo hello").then(console.log)}
+					hexpand
+					halign={Gtk.Align.CENTER}
+				>
+					<label label="Welcome to AGS!" />
+				</button> */}
+				<box $type="center" />
+				<box name="endbox" $type="end" hexpand halign={Gtk.Align.END}>
+					<menubutton class="clock">
+						<box>
+							<label class="chevron" valign={Gtk.Align.CENTER} label="" />
+							<label class="time" label={time} />
+							<label class="chevron" name="chevron-right" valign={Gtk.Align.CENTER} label="" />
+						</box>
+						<popover>
+							<Gtk.Calendar />
+						</popover>
+					</menubutton>
+				</box>
+			</centerbox>
+		</window>
+	)
 }
